8811a46187ea43c2940278c0ded39247add97c69
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2018-10-09 20:11:55 -0300

    Adicionando método POST e identificação do navegador

--- cadastroinsumo.php
+++ cadastroinsumo.php
@@ -1,6 +1,11 @@
 <html>
 
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
     <title>Cadastro Insumo</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/sticky-footer-navbar.css" rel="stylesheet">
@@ -25,27 +30,36 @@
     </header>
 
     <main role="main" class="container">
-        <form>
+        <form method="POST">
             <div class="form-group">
                 <label for="insumo">Nome do insumo</label>
-                <input type="text" class="form-control" id="nome" placeholder="Digite o nome do Insumo">
+                <input type="text" class="form-control" name="insumo" id="nome" placeholder="Digite o nome do Insumo">
             </div>
 
             <div class="form-group">
                 <label for="unidadeDeMedida">Unidade de medida</label>
-                <select class="form-control" id="unidadeDeMedida">
+                <select class="form-control" id="unidadeDeMedida" name="unidadeDeMedida">
                     <option value="" disabled selected>Selecione uma unidade de medida</option>
-                    <option>Gramas - g</option>
-                    <option>Litros - L</option>
-                    <option>Mililitro - Ml</option>
-                    <option>Quilos - Kg</option>
-                    <option>Unidade - Un</option>
+                    <option value="1">Gramas - g</option>
+                    <option value="2">Litros - L</option>
+                    <option value="3">Mililitro - Ml</option>
+                    <option value="4">Quilos - Kg</option>
+                    <option value="5">Unidade - Un</option>
                 </select>
               </div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
+            
 
+            <input type="submit" value="Salvar" class="btn btn-primary" /> 
         </form>
+
+        <table class="table">
+            <tr>
+            <th>ID</th>
+            <th>Nome Insumo</th>
+            <th>Unidade de Medida</th>
+</tr>
+                
 
     </main>
     <footer class="footer">
