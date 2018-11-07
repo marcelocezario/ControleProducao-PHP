562f65fd726376e7107bf46134469d22ce463762
Gabryel Boeira <gabryel392@gmail.com>
2018-11-07 21:54:51 -0200

    adicionado quantidade insumo

--- estoqueInsumo.php
+++ estoqueInsumo.php
@@ -1,35 +1,63 @@
 <?php
 require_once "funcoes.php";
+$id = "";
+$qtde = "";
+$id_insumo = "";
+if (!empty($_GET)) {
+    $id = $_GET['id'];
 
+    if ($_GET['acao'] == 'carregar') {
+        $insumo = buscarInsumo($id);
+        $nomeInsumo = $insumo['nomeInsumo'];
+        $unidadeMedida = $insumo['unidadeMedida'];
+    }
+    if ($_GET['acao'] == 'excluir') {
+        excluirInsumo($id);
+    }
+}
+if(!empty($_POST)) {
 
+    if (!empty($_POST['id'])){
+        editarEstoqueInsumo($_POST);
+    } else {
+        salvarEstoqueInsumo($_POST);
+    }
+}
+$insumos = listarInsumos();
 ?>
 <!DOCTYPE html>
 <?php    
         include_once("header.php");
 ?>
 <body>
-    <main>
+<main role="main" class="container">
+        <h2>Quantidade para o insumo</h2>
     <form action="estoqueInsumo.php" method="POST">
-        <br>
-        <br>
-    <div class="form-group">
-    <label class="form-group">Insumo:</label>
-    <select class="form-control" id="insumo" name="insumo"  value="<?=$insumo?>">
-    <option value="" disabled selected>Selecione um insumo </option>
+    <input type="hidden" id="id" name="id" value="<?=$id?>"/>
 
+    <div class="form-group">
+        <label for="qtde">cpf</label>
+        <input type="text" class="form-control" name="qtde" id="qtde" placeholder="Digite a quantidade">
+    </div>
+    <div class="form-group">
+    <label for="id_insumo">Insumo:</label>
+    <select class="form-control" id="id_insumo" name="id_insumo"  value="<?=$insumo?>">
+        <option value="" disabled selected>Selecione um insumo </option>
         <?php
             $resultado = listarInsumos();
         
         if(!empty($resultado)){
             foreach ($resultado as $res) {
         ?>                                             
-        <option  value="<?php echo $res['id'];?>" ><?php echo $res['nomeInsumo'];?></option> 
+        <option  value="<?=$res['id'];?>" ><?=$res['nomeInsumo'];?></option> 
         <?php      
         }
         }
         ?>
         </select>
     </div>
+    <input type="submit" value="Salvar" class="btn btn-primary" /> 
+
     </form>
     </main>
     <?php    
--- funcoes.php
+++ funcoes.php
@@ -1,6 +1,9 @@
 <?php
 require_once "conexao.php";
-
+/*
+ * Insumo começa linha 11 e ternima 75
+ * EstoqueInsumo começa linha 79 e ternima 142
+ */
 function selected( $value, $selected ){
     return $value==$selected ? ' selected="selected"' : '';
 }
@@ -29,7 +32,8 @@ function listarInsumos() {
     $stmt = $conn->prepare("select id, nomeInsumo, unidadeMedida from insumo order by nomeInsumo");
     $stmt->execute();
     $retorno = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    return $retorno;}
+    return $retorno;
+}
 
 function buscarInsumo($id) {
     $conn = conectar();
@@ -70,4 +74,70 @@ function excluirInsumo($id) {
     }
 }
 
+/*************************** ESTOQUE INSUMO ***********************************/
+
+function salvarEstoqueInsumo($estoque_insumo)  {  
+    $conn = conectar();
+
+    $stmt = $conn->prepare('INSERT INTO insumo_estoque (qtde, id_insumo)
+            VALUES(:qtde, :id_insumo)');
+
+    $stmt->bindParam(':qtde',$estoque_insumo['qtde']);
+    $stmt->bindParam(':id_insumo',$estoque_insumo['id_insumo']);
+   
+    if ($stmt->execute()){
+        return "Insumo inserido com sucesso!";
+    } else {
+        print_r($stmt->errorInfo());
+        return "erro! ";
+    }
+}
+
+function listarEstoqueInsumo() {
+    $conn = conectar();
+
+    $stmt = $conn->prepare("select ie.id, ie.qtde, i.nomeInsumo from insumo_estoque as ie left join insumo as i on i.id = ie.id_insumo");
+    $stmt->execute();
+    $retorno = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $retorno;
+}
+
+function buscarEstoqueInsumo($id) {
+    $conn = conectar();
+
+    $stmt = $conn->prepare("select id, qtde, id_insumo from insumo_estoque insumo where id = :id");
+    $stmt->bindParam(':id',$id);
+
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+function editarEstoqueInsumo($insumo){
+    $conn = conectar();
+
+    $stmt = $conn->prepare('update insumo set nomeInsumo = :nomeInsumo, unidadeMedida = :unidadeMedida where id = :id');
+    $stmt->bindParam(':id',$insumo['id']);
+    $stmt->bindParam(':nomeInsumo',$insumo['nomeInsumo']);
+    $stmt->bindParam(':unidadeMedida',$insumo['unidadeMedida']);
+
+    if ($stmt->execute()){
+        return "Insumo alterado com sucesso!";
+    } else {
+        print_r($stmt->errorInfo());
+        return "erro! ";
+    }
+}
+
+function excluirEstoqueInsumo($id) {
+    $conn = conectar();
+
+    $stmt = $conn->prepare('delete from insumo where id = :id');
+    $stmt->bindParam(':id',$id);
+    if ($stmt->execute()){
+        return "Insumo excluído com sucesso!";
+    } else {
+        print_r($stmt->errorInfo());
+        return "erro! ";
+    }
+}
 ?>
