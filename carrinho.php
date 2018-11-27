9dd72111bc4fa4b8936173ec736db4e2641682c1
gabryel <gabryel392@gmail.com>
2018-11-27 21:27:08 -0200

    teste

--- carrinho.php
+++ carrinho.php
@@ -2,9 +2,10 @@
 include_once("default/header.php");
 
 $cliente = $_SESSION['cliente'];
-print_r($cliente);
-echo "Olá Sr(a). ".$cliente['nome'].", este é seu carrinho de compras";
 
 include_once("default/navbar.php");
+
+echo "Olá Sr(a). ".$cliente['nome'].", este é seu carrinho de compras";
+
 
 ?>
