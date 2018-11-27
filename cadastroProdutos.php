4473602b07a75247f57675af27a53a2b6b08e8bb
gabryel <gabryel392@gmail.com>
2018-11-27 21:07:15 -0200

    correção

--- cadastroProdutos.php
+++ cadastroProdutos.php
@@ -132,7 +132,13 @@ include_once("default/header.php");
                         <td><?=$produto['id']?></td>
                         <td><?=$produto['nomeProduto']?></td>
                         <td><?=$produto['descricao']?></td>
+                        <?php
+                            if(!empty($produto['url'])){                                 
+                        ?>
                         <td><img src="<?=$produto['url']?>" class="rounded-circle" width="50" height="50" /></td>
+                        <?php
+                            }
+                        ?>
                         <td>
                             <a href="cadastroProdutos.php?acao=carregar&id=<?=$produto['id']?>"
                                 class="btn btn-primary">Editar
