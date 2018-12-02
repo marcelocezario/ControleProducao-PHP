7f963c8ac1a450dac3c130a9a35c65f709980d34
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2018-12-01 22:46:16 -0200

    Renomeando tabela do banco de dados

--- carrinho.php
+++ carrinho.php
@@ -9,3 +9,40 @@ echo "Olá Sr(a). ".$cliente['nome'].", este é seu carrinho de compras";
 
 
 ?>
+
+<?php 
+    require_once "funcoes/funcaoProduto.php";
+    include_once("default/header.php");
+
+    
+    if (!empty($_GET)) {
+      $idProduto = $_GET['idProduto'];
+
+      if ($_GET['acao'] == 'carregar') {
+          $produto = buscarProduto($idProduto);
+      }
+    }
+   
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+  <body>
+        <?php    
+            include_once("default/navbar.php");
+        ?>
+
+
+
+
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  
+  </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+header("location: produtos.php");
+
+?>
